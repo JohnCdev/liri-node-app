76cf85f291c2b77628bdf04e4dcbcb73b51cb7f1
JohnCdev <johncarverp@gmail.com>
2019-09-22 16:21:02 -0400

    keys, env

--- keys.js
+++ keys.js
@@ -4,3 +4,8 @@ exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+
+exports.other = {
+  omdb: process.env.omdb,
+  bit: process.env.bit
+}
--- liri.js
+++ liri.js
@@ -10,28 +10,46 @@ var method = process.argv[2];
 var input = process.argv[3]
 
 var artist = method;
+var title = method;
 
-// axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
-//     .then(function (res) {
-//         console.log(res.data);
+//commands
+// concert-this
+// spotify-this-song
+// movie-this
+// do-what-it-says
+
+//Axios Bands in Town request
+axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=" + keys.other.bit)
+    .then(function (res) {
+        console.log(res.data);
+    })
+    .catch(function (err) {
+        if (err.response) {
+
+            console.log(err.response.data);
+            console.log(err.response.status);
+            console.log(err.response.headers);
+        } else if (err.request) {
+            console.log(err.request);
+        } else {
+            console.log("Error", err.message);
+        }
+        console.log(err.config);
+    });
+
+//Axios OMBD
+// axios.get("https://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=" + keys.other.omdb)
+//     .then(function(res) {
+//         console.log(res.data)
 //     })
 //     .catch(function (err) {
-//         if (err.response) {
-
-//             console.log(err.response.data);
-//             console.log(err.response.status);
-//             console.log(err.response.headers);
-//         } else if (err.request) {
-//             console.log(err.request);
-//         } else {
-//             console.log("Error", err.message);
-//         }
-//         console.log(err.config);
+//         console.log(err)
 //     });
 
-spotify.search({ type: 'track', query: 'All the Small Things' }, function (err, res) {
-    if (err) {
-        return console.log('Error occurred: ' + err);
-    }
-    console.log(res.tracks);
-});
+//Spotify api request
+// spotify.search({ type: 'track', query: 'All the Small Things' }, function (err, res) {
+//     if (err) {
+//         return console.log('Error occurred: ' + err);
+//     }
+//     console.log(res.tracks)
+// });
